581a764d68aa84e1d9afbc64a9455ea978c57014
Patrick Owens <manbearpat@gmail.com>
2019-07-17 23:58:49 -0700

    attempting to reconcile server with handlebars and heroku

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-//require("dotenv").config();
+
 var express = require("express");
 var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-var db = require("./models");
+//var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
 ////passport stuffs
-//app.use(
+//app.use
 //  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 //);
 //app.use(passport.initialize());

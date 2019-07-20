df1bc0cdd344a8acf3136cd229e469a614f6bc1c
Patrick Owens <manbearpat@gmail.com>
2019-07-20 14:12:18 -0700

    returned axios code to server file for scraping

--- server.js
+++ server.js
@@ -33,6 +33,25 @@ app.get("/", function (req, res) {
 // Routes
 require("./routes/htmlRoutes")(app);
 
+//axios thing to scrape wsj
+axios.get("https://www.wsj.com").then(function(response) {
+
+  var $ = cheerio.load(response.data);
+  var results = [];
+  
+  $("article").each(function(i, element) {
+
+    var title = $(element).children().text();
+    var link = $(element).find("a").attr("href");
+
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+  console.log(results);
+});
 
 
 

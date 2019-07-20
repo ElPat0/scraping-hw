7d337b9202789a98405892a13634015c62ae2525
Patrick Owens <manbearpat@gmail.com>
2019-07-20 13:17:15 -0700

    removed scrape button code from server file

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,25 +1,25 @@
 //re-requiring axios
 //var axios = require("axios");
 //Scrape button executes scrape and then populates collapsible div's with article title & content
-//$("#scrape-btn").on("click", function() {
-//    //The bit that scrapes
-//axios.get("https://www.wsj.com").then(function(response) {
-//
-//    var $ = cheerio.load(response.data);
-//    var results = [];
-//    
-//    $("article").each(function(i, element) {
-//  
-//      var title = $(element).children().text();
-//      var link = $(element).find("a").attr("href");
-//  
-//      results.push({
-//        title: title,
-//        link: link
-//      });
-//    });
-//  
-//    console.log(results);
-//  });
-//    
-//});
+$("#scrape-btn").on("click", function() {
+    //The bit that scrapes
+axios.get("https://www.wsj.com").then(function(response) {
+
+    var $ = cheerio.load(response.data);
+    var results = [];
+    
+    $("article").each(function(i, element) {
+  
+      var title = $(element).children().text();
+      var link = $(element).find("a").attr("href");
+  
+      results.push({
+        title: title,
+        link: link
+      });
+    });
+  
+    console.log(results);
+  });
+    
+});
--- server.js
+++ server.js
@@ -28,29 +28,7 @@ app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/htmlRoutes")(app);
-//button click to scrape
-("#scrape-btn").on("click", function() {
-  //The bit that scrapes
-axios.get("https://www.wsj.com").then(function(response) {
 
-  var $ = cheerio.load(response.data);
-  var results = [];
-  
-  $("article").each(function(i, element) {
-
-    var title = $(element).children().text();
-    var link = $(element).find("a").attr("href");
-
-    results.push({
-      title: title,
-      link: link
-    });
-  });
-
-  console.log(results);
-});
-  
-});
 
 
 

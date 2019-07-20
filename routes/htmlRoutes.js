977623c8666b4390ee8f3428413580a9cf516e76
Patrick Owens <manbearpat@gmail.com>
2019-07-20 09:52:12 -0700

    testing server without you variable

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,15 +4,15 @@
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    let you = req.user;
+    //let you = req.user;
 
     res.render("index", { user: you });
   });
- // // Load Signup page
- // app.get("/signup", function (req, res) {
-//
- //   res.render("signup");
- // });
+ // Load Articles page
+  app.get("/signup", function (req, res) {
+
+  res.render("signup");
+  });
 //
  // // Seller public profile
  // app.get("/sellercover/:sellerid", function (req, res) {

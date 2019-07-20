adf6812395c2d5ce1fc919c23ff518a9aacfb9c1
Patrick Owens <manbearpat@gmail.com>
2019-07-20 12:58:54 -0700

    declared axios on app.js in attempt to allow button function to scrape

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,5 @@
-
+// re-requiring axios
+var axios = require("axios");
 //Scrape button executes scrape and then populates collapsible div's with article title & content
 $("#scrape-btn").on("click", function() {
     //The bit that scrapes

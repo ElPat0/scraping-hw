2aa731e45ad7f762dabbb0e3536aa82f9b24c7ec
Patrick Owens <manbearpat@gmail.com>
2019-07-20 14:09:30 -0700

    cleanup of html routes file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,33 +1,10 @@
 
 
 module.exports = function (app) {
-  // Load index page & scrape wsj
-  app.get("/", function (req, res) {
-    axios.get("https://www.wsj.com").then(function(response) {
-
-    var $ = cheerio.load(response.data);
-    var results = [];
-    
-    $("article").each(function(i, element) {
   
-      var title = $(element).children().text();
-      var link = $(element).find("a").attr("href");
   
-      results.push({
-        title: title,
-        link: link
-      });
-    });
   
-    console.log(results);
-  });
-    res.render("index");
-  });
- // Load Articles page
-  //app.get("/articles", function (req, res) {
-
- // res.render("articles");
- // });
+ 
 
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {

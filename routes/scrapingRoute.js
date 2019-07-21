495c87e2409ed8de05342fa638031005466f00a4
Patrick Owens <manbearpat@gmail.com>
2019-07-20 22:18:08 -0700

    testing

--- routes/scrapingRoute.js
+++ routes/scrapingRoute.js
@@ -2,36 +2,31 @@
 
 var axios = require("axios");
 var cheerio = require("cheerio");
-var db = require("../models")
-
+var db = require("../models");
 module.exports = function (app) {
-
-app.get("/scrape", function (req, res){
-
-    axios.get("http://www.wsj.com/").then(function(response) {
-
-    var $ = cheerio.load(response.data);
-
-    $("article").each(function(i, element) {
+  app.get("/scrape", function (req, res) {
+    axios.get("http://www.wsj.com/").then(function (response) {
+      var $ = cheerio.load(response.data);
+      var array = [];
+      $("article").each(function (i, element) {
         var result = {};
-
-        result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-        //new article
-        db.Article.create(result)
-        .then(function(dbArticle) {
+        result.title = $(element)
+          .find(".WSJTheme--headline--19_2KfxG")
+          .text();
+        result.link = $(element)
+          .find("a")
+          .attr("href");
+        array.push(result);
+      });
+      //new article
+      db.Article.insertMany(array)
+        .then(function (dbArticle) {
           res.json(dbArticle);
-            console.log(dbArticle);
+          console.log(dbArticle);
         })
-        .catch(function(err) {
-            console.log(err);
+        .catch(function (err) {
+          console.log(err);
         });
-    }); 
+    });
   });
-});
 }

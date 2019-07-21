393b09120d09de7883fc074bcbf92b840643c02e
Patrick Owens <manbearpat@gmail.com>
2019-07-20 17:02:20 -0700

    modified scraping route

--- routes/scrapingRoute.js
+++ routes/scrapingRoute.js
@@ -1,10 +1,10 @@
-
+module.exports = function (app) {
 
 app.get("/scrape", function (req, res){
     axios.get("https://www.wsj.com").then(function(response) {
   
-  console.log("wsj");
-  console.log(response);
     res.json(response);
+    console.log(response);
   });
 });
+}
